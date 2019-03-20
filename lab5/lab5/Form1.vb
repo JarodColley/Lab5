337cf3d02a82e48524bee0651c52ec2586003688
JarodColley <47607281+JarodColley@users.noreply.github.com>
2019-03-20 13:40:11 -0400

    coded opening a file

--- lab5/lab5/Form1.vb
+++ lab5/lab5/Form1.vb
@@ -1,13 +1,40 @@
 ﻿Option Strict On
+Imports System.IO
 Public Class frmMain
-    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
-        Application.Exit()
-    End Sub
+
+
+
+
 
     Private Sub OpenCtrlOToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenCtrlOToolStripMenuItem.Click
-        If (OpenFileDialog1.ShowDialog() = DialogResult.OK) Then
+        OpenFileDialog1.Filter = "Text Files|*.txt"
+        Try
+            If (OpenFileDialog1.ShowDialog() = DialogResult.OK) Then
+                Dim strFullPath As String = OpenFileDialog1.FileName
+                Dim fileRead As New FileStream(strFullPath, FileMode.Open, FileAccess.Read)
+                Dim reader As New StreamReader(fileRead)
+                txtMain.Text = reader.ReadToEnd()
+                reader.Close()
+            End If
+        Catch ex As Exception
+            txtMain.Text = "error"
+        End Try
 
-        End If
+    End Sub
 
+
+
+
+    Private Sub SaveCtrlSToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveCtrlSToolStripMenuItem.Click
+
+    End Sub
+
+    Public Sub Save()
+
+    End Sub
+
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
     End Sub
 End Class
